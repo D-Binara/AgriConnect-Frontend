11d9af5bc4bb9324a9484d91e0dede0d828bdabe
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-09-10 21:00:32 +0530

    First page back update

--- agri_connect/lib/main.dart
+++ agri_connect/lib/main.dart
@@ -32,18 +32,27 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, this is ${widget.title}!',
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'lib/assets/firstback.jpg', // Path to your background image
+            fit: BoxFit.cover, // Cover the entire screen
           ),
-        ),
+          Container(
+            color: Colors.transparent, // Make sure background is not obstructed
+          ),
+          Center(
+            child: Text(
+              'Hello, this is ${widget.title}!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
