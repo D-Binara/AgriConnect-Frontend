06ea14b376ece4465e704595aed3374b1513d259
D-Binara <dbkaveesha@gmail.com>
2024-09-24 21:59:46 +0530

    connect crop recommandation and prediction part

---
+++ agri_connect/lib/services/prediction_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+
+class PredictionService {
+  static const String apiUrl = 'http://192.168.8.186:8000/prediction';
+
+  static Future<Map<String, dynamic>?> predictDisease(XFile image) async {
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
+      request.files.add(await http.MultipartFile.fromPath('imageFile', image.path));
+
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        final responseData = await http.Response.fromStream(response);
+        return jsonDecode(responseData.body);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error predicting disease: $e');
+      return null;
+    }
+  }
+}
