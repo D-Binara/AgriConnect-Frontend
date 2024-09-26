c1f419c479d93319d83b9f73695d150407fb61d3
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-09-26 23:21:45 +0530

    des

--- agri_connect/lib/widgets/input_field.dart
+++ agri_connect/lib/widgets/input_field.dart
@@ -19,15 +19,16 @@ class InputField extends StatelessWidget {
     return TextField(
       controller: controller,
       obscureText: isPassword,
-      style: const TextStyle(color: Colors.white), // Make text white
+      style: const TextStyle(
+          color: Color.fromARGB(199, 46, 46, 46)), // Make text white
       decoration: InputDecoration(
-        prefixIcon: Icon(icon, color: Colors.white),
+        prefixIcon: Icon(icon, color: Color.fromARGB(200, 49, 49, 49)),
         hintText: hintText,
-        hintStyle: const TextStyle(color: Color.fromARGB(180, 255, 255, 255)),
+        hintStyle: const TextStyle(color: Color.fromARGB(110, 59, 59, 59)),
         border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(10)),
           borderSide: BorderSide(
-            color: Color.fromARGB(255, 255, 255, 255),
+            color: Color.fromARGB(190, 97, 97, 97),
             width: 2.0,
           ),
         ),
